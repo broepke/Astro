34b255a33c3ecaadc7edb970e03c3fdf7c9a4d41
Brian Roepke <broepke@gmail.com>
2022-11-13 10:19:08 -0600

    Minor formatting changes

--- dags/example_dag.py
+++ dags/example_dag.py
@@ -11,8 +11,10 @@ def my_custom_function(ts,**kwargs):
     This can be any python code you want and is called from the python operator. The code is not executed until
     the task is run by the airflow scheduler.
     """
-    print(f"I am task number {kwargs['task_number']}. This DAG Run execution date is {ts} and the current time is {datetime.now()}")
-    print('Here is the full DAG Run context. It is available because provide_context=True')
+    print(f"Task number: {kwargs['task_number']}.")
+    print(f"DAG Run execution date: {ts}.")
+    print(f"Current time: {datetime.now()}")
+    print("Here is the full DAG Run context.")
     print(kwargs)
 
 
